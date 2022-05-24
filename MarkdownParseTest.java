19c062378181ff9ff17728bc5147ef309f984f32
redagent750 <37556745+redagent750@users.noreply.github.com>
2022-05-23 16:41:08 -0800

    Added them cool test cases :)

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -23,4 +23,27 @@ public class MarkdownParseTest {
         ArrayList<String> links = new MarkdownParse().getLinks(content);
 	    assertEquals(List.of(),links); 
     }
+
+    @Test
+    public void snippetTest1() throws IOException{
+        Path fileName = Path.of("C:/Users/kenny/Documents/GitHub/markdown-parser/snippet1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = new MarkdownParse().getLinks(content);
+	    assertEquals(List.of("`google.com","google.com","ucsd.edu"),links); 
+    }
+    @Test
+    public void SnippetTest2() throws IOException{
+        Path fileName = Path.of("C:/Users/kenny/Documents/GitHub/markdown-parser/test-file2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = new MarkdownParse().getLinks(content);
+	    assertEquals(List.of("a.com","a.com(())","example.com"),links); 
+    }
+    @Test
+    public void snippetTest3() throws IOException{
+        Path fileName = Path.of("C:/Users/kenny/Documents/GitHub/markdown-parser/snippet3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = new MarkdownParse().getLinks(content);
+	    assertEquals(List.of("https://www.twitter.com","https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule",
+        "https://cse.ucsd.edu/"),links); 
+    }
 }
